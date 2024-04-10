ef9cd640be691a28bf2175dbe335cd3f2a7e4205
SciByteHub <164918540+SciByteHub@users.noreply.github.com>
2024-04-10 21:39:43 +0200

    Rozwiązania zadań

---
+++ Rozwiązania zadań tydzień 2/Zadanie_1.py
@@ -0,0 +1,13 @@
+"""
+Zadanie 1
+Stwórz zmienną „name”, „sport” oraz „age” i przypisz im stosowne, wybrane przez
+siebie wartości. Następnie za pomocą funkcji „print” wypisz zdanie:
+„Cześć, jestem <name>. Mam <age> lat. Moim ulubionym sportem jest <sport>”
+"""
+
+"""Definiujemy zmienne"""
+name = "Mateusz"
+sport = "koszykówka"
+age = 23
+
+print(f"Cześć, jestem {name}. Mam {age} lat. Moim ulubionym sportem jest {sport}.") # za pomocą funkcji "print" wypisuję w terminalu zdanie ze zmiennymi
---
+++ Rozwiązania zadań tydzień 2/Zadanie_2b.py
@@ -0,0 +1,14 @@
+"""
+Zadanie 2b
+Zainicjalizuj zmienną number1 i przypisz jej wartość 5, następnie stwórz zmienną
+number2 i przypisz jej wartość „5”. Stwórz zmienną „i_need_a_cup_of_coffee” i
+przypisz do niej działanie dodawania wcześniej zainicjalizowanych zmiennych.
+Co zaobserwujesz?
+"""
+"""Definiuję zmienne"""
+number1 = 5
+number2 = "5"
+
+I_need_a_cup_of_coffee = number1 + number2 
+print(I_need_a_cup_of_coffee) # kod nie działa, ponieważ próbujemy dodać zmienną typu int (liczba całkowita) oraz zmienną typu str (łańcuch znaków)
+
