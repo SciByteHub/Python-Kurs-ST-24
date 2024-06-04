f88f797510a75ad7f44110263dd81ab03f7f0fbf
vermenea <vermenea@gmail.com>
2024-06-04 21:51:22 +0200

    board class with demo sizing methods

--- Grupa_2/main.py
+++ Grupa_2/main.py
@@ -5,4 +5,15 @@ class Cell:
 
     def __str__(self):
         return "*" if self.alive else "."
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.grid = [[Cell(True, False) for _ in range(self.width)] for _ in range(self.height)]
+    
+    def display(self):
+        for row in self.grid:
+            print(" ".join(str(cell) for cell in row))
 
+    def __str__(self):
+        return '\n'.join(' '.join(str(cell) for cell in row) for row in self.grid)
