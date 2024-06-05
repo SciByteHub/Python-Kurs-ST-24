df5b1a70071b7711822518ba4f218429e5c1bc9f
AdamBorys1 <adamborys2002@wp.pl>
2024-06-05 19:04:59 +0200

    przykładowy aminokwas

--- Grupa_1/main.py
+++ Grupa_1/main.py
@@ -1,1 +1,16 @@
-test dzialania gita
+# wyświetlanie najliczniejszego aminokwasu w białku
+from rdkit import Chem
+from rdkit.Chem import Draw
+
+fenyloalanina_smiles = "NC(Cc1ccccc1)C(=O)O"
+fenyloalanina_molecule = Chem.MolFromSmiles(fenyloalanina_smiles)
+
+image = Draw.MolToImage(fenyloalanina_molecule, size=(300, 300))
+image.show()
+
+# porównanie sekwencji białek, szacowanie podobieństwa
+
+
+
+
+# oznaczenie hydrofobowych i hydrofilowych fragmentów białka
