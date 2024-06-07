4515b3cb6d004dfe87497d3a9fa98c1df4d48719
vermenea <vermenea@gmail.com>
2024-06-07 12:11:05 +0200

    updated cell class and modyfied bord grid

--- Grupa_2/main.py
+++ Grupa_2/main.py
@@ -1,7 +1,18 @@
+#libs
+import random
+
+#defining classes
 class Cell:
-    def __init__(self, alive, dead):
+    def __init__(self, alive):
         self.alive = alive
-        self.dead = dead
+        
+    def update_status(self, alive_neighbors):
+        if self.alive:
+            if alive_neighbors < 2 or alive_neighbors > 3:
+                self.alive = False
+        else:
+            if alive_neighbors == 3:
+                self.alive = True
 
     def __str__(self):
         return "*" if self.alive else "."
@@ -9,7 +20,7 @@ class Board:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.grid = [[Cell(True, False) for _ in range(self.width)] for _ in range(self.height)]
+        self.grid = [[Cell(random.choice([True, False])) for _ in range(self.width)] for _ in range(self.height)]
     
     def display(self):
         for row in self.grid:
@@ -17,3 +28,4 @@ class Board:
 
     def __str__(self):
         return '\n'.join(' '.join(str(cell) for cell in row) for row in self.grid)
+
