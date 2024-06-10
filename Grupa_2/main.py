91ded9f5f7b7eb347abc70d2d11fedd01c2044bc
vermenea <vermenea@gmail.com>
2024-06-10 13:40:13 +0200

    changed cell's display

--- Grupa_2/main.py
+++ Grupa_2/main.py
@@ -16,7 +16,7 @@ class Cell:
                 self.alive = True
 
     def __str__(self):
-        return "*" if self.alive else "."
+        return "■" if self.alive else " "
 class Board:
     def __init__(self, width, height):
         self.width = width
@@ -63,23 +63,23 @@ if __name__ == "__main__":
     board.display()
     
     while True:
-        command = input((f"In a moment game of life will begin, if you would like to quit the game press ctrl + c at once. Begin ? (yes/no): "))
-        if command.lower() == "yes":
-            while True:
+        # command = input((f"In a moment game of life will begin, if you would like to quit the game press ctrl + c at once. Begin ? (yes/no): "))
+        # if command.lower() == "yes":
+            # while True:
                 
                 print(f"\n next generation: ")
                 board.update_board()
                 board.display()
                 time.sleep(5)
-                update = input(f"Press ENTER if you wish to continue or q to quit the game: ")
-                if update == 'q':
-                    print("Exiting the game. Goodbye!")
-                    break
-        elif command == 'q' or command == 'no':
-            print("Exiting the game. Goodbye!")
-            break
-        else:
-            print("Invalid input. Please type 'yes' to start or 'q' to quit.")
+        #         update = input(f"Press ENTER if you wish to continue or q to quit the game: ")
+        #         if update == 'q':
+        #             print("Exiting the game. Goodbye!")
+        #             break
+        # elif command == 'q' or command == 'no':
+        #     print("Exiting the game. Goodbye!")
+        #     break
+        # else:
+        #     print("Invalid input. Please type 'yes' to start or 'q' to quit.")
     
         
         
